c974a44a967fdd9a42e3931c14a2bdb819e4e154
Deepak Kumar Dewani <kumard8308887772@gmail.com>
2024-01-14 02:39:04 +0530

    ADD: pinia

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,22 @@
-// Plugins
+import "@/assets/main.scss";
+
 import { registerPlugins } from "@/plugins";
+import { createPinia } from "pinia";
+import { createApp } from "vue";
+import router from "./router";
 
 // Components
 import App from "./App.vue";
 
-// Composables
-import { createApp } from "vue";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faUser } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+library.add(faUser);
 
-const app = createApp(App);
+const pinia = createPinia();
+const app = createApp(App).component("font-awesome-icon", FontAwesomeIcon);
 
 registerPlugins(app);
-
+app.use(pinia);
+app.use(router);
 app.mount("#app");
