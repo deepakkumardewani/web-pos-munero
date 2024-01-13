abe8df883896888457f24ea4dccd6a417b2169c5
Deepak Kumar Dewani <kumard8308887772@gmail.com>
2024-01-13 12:31:18 +0530

    ADD: footer

---
+++ src/components/TheFooter.vue
@@ -0,0 +1,12 @@
+<template>
+  <v-footer app absolute color="grey" min-height="60">
+    <v-row justify="center" no-gutters>
+      <v-col class="text-center mt-4" cols="12">
+        © {{ new Date().getFullYear() }} — <strong>Munero.</strong> All Rights
+        Reserved.
+      </v-col>
+    </v-row>
+  </v-footer>
+</template>
+
+<script setup lang="ts"></script>
