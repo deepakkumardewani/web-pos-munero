81765165c0cf8b3fc25c4319345847d47f1e95b7
Deepak Kumar Dewani <kumard8308887772@gmail.com>
2024-01-14 02:33:22 +0530

    add GiftCard template

---
+++ src/components/GiftCard.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-card
+    class="my-3 bg-elementBg md:tw-w-[300px] tw-w-[400px]"
+    height="350"
+    :ripple="false"
+  >
+    <v-img
+      height="200"
+      :src="card.cardFaceImage"
+      cover
+      class="text-white"
+    ></v-img>
+    <v-card-text class="bg-elementBg">
+      <div class="mb-3 text-h6">
+        <b>{{ card.brand }}</b>
+      </div>
+      <div>
+        Available from <b>{{ currency }} {{ card.fromValue }}</b> to
+        <b>{{ currency }} {{ card.toValue }}</b>
+      </div>
+
+      <div class="text-center tw-my-5">
+        <v-btn color="info" @click="order(card)">Buy</v-btn>
+      </div>
+    </v-card-text>
+  </v-card>
+</template>
