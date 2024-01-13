7e7f5694dc228525e58845c0f50e21af4fcfaab6
Deepak Kumar Dewani <kumard8308887772@gmail.com>
2024-01-13 12:31:04 +0530

    ADD: top nav links

---
+++ src/components/TheTopNav/TopNavMenu.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import { ref } from "vue";
+const pagesLinks = ref([
+  {
+    id: "catalogue",
+    name: "Catalogue",
+    path: "/catalogue",
+  },
+  {
+    id: "orders",
+    name: "Orders",
+    path: "/orders",
+  },
+  {
+    id: "ordering",
+    name: "Ordering",
+    path: "/ordering",
+  },
+]);
+</script>
+<template>
+  <template v-for="page in pagesLinks" :key="page.id">
+    <v-btn variant="text" :to="page.path">
+      {{ page.name }}
+    </v-btn>
+  </template>
+</template>
