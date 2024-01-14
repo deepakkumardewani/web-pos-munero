dde1c83fba044ff42ab3f882de7df9377f397c0d
Deepak Kumar Dewani <kumard8308887772@gmail.com>
2024-01-14 23:59:47 +0530

    ADD: more fields & validation

--- src/pages/OrderingPage.vue
+++ src/pages/OrderingPage.vue
@@ -1,19 +1,43 @@
 <script setup lang="ts">
 import { useApi } from "@/composables/api";
 import VueQrcode from "vue-qrcode";
-
 import { onMounted, ref } from "vue";
-import { useOrderStore } from "@/stores/order";
-const { post, get } = useApi();
-const orderStore = useOrderStore();
 
-const giftCards = ref<any[]>([]);
+import { useLocal } from "@/composables/local";
+
+const { getLocal } = useLocal();
+import { useStore } from "@/stores";
+import { countries, languages } from "@/utils";
+const { post, get } = useApi();
+const store = useStore();
+
+const rules = {
+  required: (value: string) => !!value || "Required.",
+};
 const isQRVisible = ref<boolean>(false);
 const claimURL = ref<string>("");
 const firstName = ref<string>("");
 const lastName = ref<string>("");
-const amount = ref<number>(0);
-onMounted(async () => {});
+const amount = ref<number>();
+const cardItemId = ref<string>("");
+const cardItems = ref<any[]>([]);
+const contactNumber = ref<string>("");
+const deliveryChannel = ref<string>("api");
+const smsMobileNumber = ref<string>("");
+const language = ref<string>("EN");
+const country = ref<string>("AE");
+const emailAddress = ref<string>("");
+const countryCodes = ref<string[]>(countries);
+const languageCodes = ref<string[]>(languages);
+onMounted(async () => {
+  if (store.card) {
+    cardItemId.value = store.card.id;
+  }
+  const localCards = await getLocal("giftCards");
+  if (localCards && Object.keys(localCards).length !== 0) {
+    cardItems.value = localCards.map((item: any) => item.id);
+  }
+});
 
 function addAmount(amt: string) {
   amount.value = Number(amt);
@@ -30,11 +54,15 @@ async function placeOrder() {
     customerName: firstName.value + lastName.value,
     firstName: firstName.value,
     lastName: lastName.value,
-    deliveryChannel: "api",
     referenceNo: uniqueId,
+    deliveryChannel: deliveryChannel.value,
+    smsMobileNumber: smsMobileNumber.value,
+    emailAddress: emailAddress.value,
+    countryCode: country.value,
+    languageCode: language.value,
     lineItems: [
       {
-        cardItemId: orderStore.order.id,
+        cardItemId: cardItemId.value,
         value: amount.value,
       },
     ],
@@ -42,6 +70,7 @@ async function placeOrder() {
 
   const { data, isFinished, error, isFetching } = await post(url, body);
   if (isFinished.value && !error.value) {
+    reset();
     generateQRForClaimURL(uniqueId);
   }
 }
@@ -54,10 +83,25 @@ async function generateQRForClaimURL(id: string) {
     isQRVisible.value = true;
   }
 }
+
+function reset() {
+  firstName.value = "";
+  lastName.value = "";
+  amount.value = 0;
+  cardItemId.value = "";
+  contactNumber.value = "";
+  deliveryChannel.value = "api";
+  smsMobileNumber.value = "";
+  language.value = "EN";
+  country.value = "AE";
+  emailAddress.value = "";
+  claimURL.value = "";
+}
 </script>
 <template>
+  <Heading text="New Order" />
   <div class="tw-flex tw-justify-center tw-items-center">
-    <v-card width="500">
+    <v-card width="500" class="my-3">
       <v-card-text>
         <v-row>
           <v-col cols="6">
@@ -66,7 +110,14 @@ async function generateQRForClaimURL(id: string) {
               label="First Name"
               variant="outlined"
               placeholder="First Name"
-            ></v-text-field>
+              :rules="[rules.required]"
+              required
+            >
+              <template #label>
+                <span class="tw-text-red-500"><strong>* </strong></span>First
+                Name
+              </template>
+            </v-text-field>
           </v-col>
           <v-col cols="6">
             <v-text-field
@@ -74,7 +125,100 @@ async function generateQRForClaimURL(id: string) {
               label="Last Name"
               variant="outlined"
               placeholder="Last Name"
+              :rules="[rules.required]"
+            >
+              <template #label>
+                <span class="tw-text-red-500"><strong>* </strong></span>Last
+                Name
+              </template></v-text-field
+            >
+          </v-col>
+        </v-row>
+        <v-row justify="center" align="center">
+          <v-col cols="6">
+            <v-text-field
+              v-model="contactNumber"
+              type="number"
+              label="Contact Number"
+              variant="outlined"
+              placeholder="Contact Number"
+              hide-spin-buttons
             ></v-text-field>
+          </v-col>
+          <v-col cols="6">
+            <v-select
+              variant="outlined"
+              label="Delivery Channel"
+              v-model="deliveryChannel"
+              :items="['email', 'sms', 'api']"
+            >
+            </v-select>
+          </v-col>
+        </v-row>
+        <v-row justify="center" align="center">
+          <v-col cols="6">
+            <v-text-field
+              v-model="smsMobileNumber"
+              type="number"
+              label="SMS Mobile Number"
+              variant="outlined"
+              placeholder="SMS Mobile Number"
+              hide-spin-buttons
+            >
+              <template v-if="deliveryChannel === 'sms'" #label>
+                <span class="tw-text-red-500"><strong>* </strong></span>SMS
+                Mobile Number
+              </template></v-text-field
+            >
+          </v-col>
+          <v-col cols="6">
+            <v-text-field
+              v-model="emailAddress"
+              label="Email Address"
+              variant="outlined"
+              placeholder="Email Address"
+            >
+              <template v-if="deliveryChannel === 'email'" #label>
+                <span class="tw-text-red-500"><strong>* </strong></span>Email
+                Address
+              </template>
+            </v-text-field>
+          </v-col>
+        </v-row>
+        <v-row justify="center" align="center">
+          <v-col cols="6">
+            <v-select
+              variant="outlined"
+              label="Country codes"
+              v-model="country"
+              :items="countryCodes"
+            >
+            </v-select>
+          </v-col>
+          <v-col cols="6">
+            <v-select
+              variant="outlined"
+              label="Language Codes"
+              v-model="language"
+              :items="languageCodes"
+            >
+            </v-select>
+          </v-col>
+        </v-row>
+        <v-row justify="center" align="center">
+          <v-col cols="12">
+            <v-select
+              variant="outlined"
+              label="Card Item ID"
+              v-model="cardItemId"
+              :rules="[rules.required]"
+              :items="cardItems"
+            >
+              <template #label>
+                <span class="tw-text-red-500"><strong>* </strong></span>Card
+                Item ID
+              </template>
+            </v-select>
           </v-col>
         </v-row>
         <v-row justify="center" align="center">
@@ -82,17 +226,26 @@ async function generateQRForClaimURL(id: string) {
             <div class="tw-my-3 text-center">
               <v-text-field
                 v-model="amount"
+                type="number"
                 label="Amount"
                 variant="outlined"
                 placeholder="amount"
                 hint="enter your amount between $1 to $1000 or select one from below"
+                hide-spin-buttons
+                :rules="[rules.required]"
               >
+                <template #label>
+                  <span class="tw-text-red-500"><strong>* </strong></span>Amount
+                </template>
               </v-text-field>
             </div>
             <div class="text-center">
               <v-chip class="tw-mx-2" @click="addAmount('10')">$10</v-chip>
+              <v-chip class="tw-mx-2" @click="addAmount('20')">$20</v-chip>
               <v-chip class="tw-mx-2" @click="addAmount('50')">$50</v-chip>
               <v-chip class="tw-mx-2" @click="addAmount('100')">$100</v-chip>
+              <v-chip class="tw-mx-2" @click="addAmount('500')">$500</v-chip>
+              <v-chip class="tw-mx-2" @click="addAmount('1000')">$1000</v-chip>
             </div>
             <div class="tw-my-8 text-center">
               <v-btn color="info" @click="placeOrder()">Place Order</v-btn>
