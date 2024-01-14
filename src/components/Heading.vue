4527a5651ce5923614b1f4282cd6093fabe2df2e
Deepak Kumar Dewani <kumard8308887772@gmail.com>
2024-01-15 00:02:33 +0530

    ADD: Heading component

---
+++ src/components/Heading.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="text-center">
+    <div class="tw-text-3xl tw-font-bold">
+      {{ text }}
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+defineProps({
+  text: {
+    type: String,
+    required: true,
+  },
+});
+</script>
