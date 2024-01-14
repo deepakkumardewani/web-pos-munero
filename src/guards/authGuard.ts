9b38f1a88dd87539c282b03706fcf64fd6b3a695
Deepak Kumar Dewani <kumard8308887772@gmail.com>
2024-01-15 00:01:34 +0530

    ADD: moment for expiry date comparison

--- src/guards/authGuard.ts
+++ src/guards/authGuard.ts
@@ -1,24 +1,24 @@
 import { useToken } from "@/composables/token";
+import moment from "moment";
 import type { NavigationGuard } from "vue-router";
-
 export const authGuard: NavigationGuard = async (to, from, next) => {
   const { getToken } = useToken();
+
+  console.log("authGuard");
 
   const tokenData = await getToken();
   if (tokenData) {
     const expiryDate = tokenData.expireDate;
     // convert expiryDate from UTC to local time
-    const expiryDateLocal = new Date(expiryDate).toLocaleString();
+    const expiryDateLocal = moment.utc(expiryDate).local();
 
     // check if current time is past expiry date
-    if (new Date() > new Date(expiryDateLocal)) {
+    if (moment().isSameOrAfter(expiryDateLocal)) {
       next("/login");
     } else {
       next();
     }
   } else {
-    console.log("here");
-
     next("/login");
   }
 };
