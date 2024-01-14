4874982eb179b240367d5e23b09998fb7ec8f385
Deepak Kumar Dewani <kumard8308887772@gmail.com>
2024-01-14 23:58:33 +0530

    ADD: skeleton loader

--- src/pages/CataloguePage.vue
+++ src/pages/CataloguePage.vue
@@ -7,13 +7,13 @@ const { setLocal, getLocal } = useLocal();
 const { get } = useApi();
 
 const giftCards = ref<any[]>([]);
+const loading = ref<boolean>(true);
 
 onMounted(async () => {
-  console.log("Catalog page mounted");
   const localCards = await getLocal("giftCards");
   if (localCards && Object.keys(localCards).length !== 0) {
     console.log("Fetching from local storage");
-
+    loading.value = false;
     giftCards.value = localCards;
   } else {
     console.log("Fetching from API");
@@ -27,16 +27,24 @@ onMounted(async () => {
     };
     const { data, isFinished, error, isFetching } = await get(url, params);
     if (isFinished.value) {
+      loading.value = isFetching.value;
       giftCards.value = data.value.items;
       setLocal("giftCards", giftCards.value);
-      console.log(giftCards.value);
     }
   }
 });
 </script>
 <template>
   <div>
-    <div class="pa-10 d-flex flex-wrap justify-space-around">
+    <Heading text="My Catalogue" />
+    <div v-if="loading" class="pa-10 d-flex flex-wrap justify-space-around">
+      <v-skeleton-loader
+        class="my-3 md:tw-w-[300px] tw-w-[400px]"
+        v-for="_ in 5"
+        type="card"
+      ></v-skeleton-loader>
+    </div>
+    <div v-else class="pa-10 d-flex flex-wrap justify-space-around">
       <GiftCard v-for="card in giftCards" :key="card.id" :card="card" />
     </div>
   </div>
