b28448bf275c09395e2744b2ac81c866b91f53c1
Deepak Kumar Dewani <kumard8308887772@gmail.com>
2024-01-15 21:03:00 +0530

    remove console

--- src/guards/authGuard.ts
+++ src/guards/authGuard.ts
@@ -3,9 +3,6 @@ import moment from "moment";
 import type { NavigationGuard } from "vue-router";
 export const authGuard: NavigationGuard = async (to, from, next) => {
   const { getToken } = useToken();
-
-  console.log("authGuard");
-
   const tokenData = await getToken();
   if (tokenData) {
     const expiryDate = tokenData.expireDate;
